a1373b21861c1e16d6a65aa71f040a4193542569
大勇士 <977633983@qq.com>
2022-05-17 12:31:13 +0800

    Site updated: 2022-05-17 12:31:04

--- bash modify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir="_posts"
-for file in `ls ${dir} | grep '.md'`;do
-    content=$(cat ${dir}/${file}| head -n 10 | grep 'date: ')
-    datestr=$(echo "$content" | awk '{print $2" "$3}')
-    newcontent="updated: "$datestr
-    sed -i "/$content/a\\$newcontent" ${dir}/${file}
-done
